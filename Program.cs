8b540996c09eca4b0cf2c227ba4841fd05432ed8
user <user@WIN-LDEU15BD8LL>
2025-10-12 15:20:14 +0500

    Initial commit: all source and EXE (with LFS)

---
+++ Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace HelloWorld
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadKey();
+        }
+    }
+}
+
+
